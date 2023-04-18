74830f0e73591ef920b59b60e0059595239006ca
huiping.liu <huiping.liu@silverkeyz.com>
2023-04-18 15:31:40 +0800

    add some comment about reference qualifiers

--- Part1-BasicFeaturesOfMoveSemantics/Chapter05-OverloadingOnReferenceQualifiers/OverloadingOnQualifiers/refqual.h
+++ Part1-BasicFeaturesOfMoveSemantics/Chapter05-OverloadingOnReferenceQualifiers/OverloadingOnQualifiers/refqual.h
@@ -6,6 +6,8 @@ class C
 {
 public:
 #if __cplusplus < 201103L
+
+    // before C++11, only have const and non-const
     void foo()
     {
         println( __PRETTY_FUNCTION__ );
@@ -15,6 +17,7 @@ public:
         println( __PRETTY_FUNCTION__ );
     }
 #else
+    // Usually, we have two or three of these oveloads, such as using && and const&(and &) for getters.
     void foo() &
     {
         println( __PRETTY_FUNCTION__ );
